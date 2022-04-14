a3844b6f7d2edead18d4a1140fe03273573d63ab
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-14 13:44:49 -0600

    Tracker Resource controller created and DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackerModule } from './tracker/tracker.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TrackerModule,
+    MongooseModule.forRoot('mongodb://localhost/collection-tracker'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
