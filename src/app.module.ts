6d670b6d3bfee4feac8d28d19d3b0f1ec11bc223
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-17 15:25:19 -0600

    Conection to mongodb Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackerModule } from './tracker/tracker.module';
-
+//MongooseModule.forRoot('mongodb://localhost/collection-tracker'),
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://bdd_usuario:ePKdySsTnGIv5K2h@cluster0.wtzw7.mongodb.net/trackerdb?retryWrites=true&w=majority'
+    ),
     TrackerModule,
-    MongooseModule.forRoot('mongodb://localhost/collection-tracker'),
   ],
   controllers: [AppController],
   providers: [AppService],
