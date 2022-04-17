958d6dd7bec2e2c49667d39b4ea6b558c54cb0ee
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-17 16:36:24 -0600

    heroku config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(9000);
+  await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
