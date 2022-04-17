8b42b53e9c840421818e508f61c0e2ebba3a5487
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-17 03:20:45 -0600

    Change update method

--- src/schemas/collection.schema.ts
+++ src/schemas/collection.schema.ts
@@ -28,6 +28,9 @@ export class Collection {
 
   @Prop()
   image: string;
+
+  @Prop()
+  __v: number;
   
 }
 
--- src/tracker/tracker.controller.ts
+++ src/tracker/tracker.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { TrackerService } from './tracker.service';
 import { CreateTrackerDto } from './dto/create-tracker.dto';
 import { UpdateTrackerDto } from './dto/update-tracker.dto';
@@ -22,7 +31,7 @@ export class TrackerController {
     return this.trackerService.findById(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateTrackerDto: UpdateTrackerDto) {
     return this.trackerService.update(id, updateTrackerDto);
   }
